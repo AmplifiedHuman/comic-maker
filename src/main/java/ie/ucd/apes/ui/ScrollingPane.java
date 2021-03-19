e3b71b591b653eaafc5baf8c7de7a8764c78cd93
AmplifiedHuman <amplifiedhuman@gmail.com>
2021-03-19 14:37:47 +0000

    fixed scrolling pane issue - related to #4

--- src/main/java/ie/ucd/apes/ui/ScrollingPane.java
+++ src/main/java/ie/ucd/apes/ui/ScrollingPane.java
@@ -2,26 +2,14 @@ package ie.ucd.apes.ui;
 
 
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 
-public class ScrollingPane extends VBox {
-    GridPane panelList = new GridPane();
+public class ScrollingPane extends ScrollPane {
     public ScrollingPane() {
-        ScrollPane sp= new ScrollPane();
-        panelList.setBorder(new Border(new BorderStroke(Color.BLACK,
-                BorderStrokeStyle.SOLID, CornerRadii.EMPTY,BorderWidths.DEFAULT)));
-        panelList.setMaxWidth(800);
-        panelList.setMinWidth(800);
-        panelList.setMaxHeight(100);
-        panelList.setMinHeight(100);
-        sp.setContent(panelList);
-        sp.setPrefWidth(800);
-        sp.setPrefHeight(250);
-        sp.isPannable();
-        sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        this.getChildren().add(panelList);
-        this.getChildren().add(sp);
+        this.setPrefWidth(800);
+        this.setPrefHeight(250);
+        this.isPannable();
+        this.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        this.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        this.setFitToHeight(true);
     }
 }
