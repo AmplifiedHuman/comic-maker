a3a7e49e1b5c1491c3da554444f505643d91dfbc
AmplifiedHuman <amplifiedhuman@gmail.com>
2021-03-19 16:51:38 +0000

    minor fixes to scrollbar - related to #4

--- src/main/java/ie/ucd/apes/ui/ScrollingPane.java
+++ src/main/java/ie/ucd/apes/ui/ScrollingPane.java
@@ -2,14 +2,19 @@ package ie.ucd.apes.ui;
 
 
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 public class ScrollingPane extends ScrollPane {
     public ScrollingPane() {
         this.setPrefWidth(800);
-        this.setPrefHeight(250);
+        this.setPrefHeight(200);
+        this.setMinHeight(200);
         this.isPannable();
         this.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         this.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         this.setFitToHeight(true);
+        this.setFitToWidth(true);
+        HBox.setHgrow(this, Priority.ALWAYS);
     }
 }
