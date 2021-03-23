ddda6cc3aa189cb5c5acc0c5d12086715e0862fe
AmplifiedHuman <amplifiedhuman@gmail.com>
2021-03-23 03:03:21 +0000

    added JUnit dependencies

---
+++ src/main/java/ie/ucd/apes/Apes.java
@@ -0,0 +1,7 @@
+package ie.ucd.apes;
+
+public class Apes {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
--- src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module ie.ucd.apes {
-    requires javafx.controls;
-    requires java.desktop;
-    exports ie.ucd.apes;
-}
